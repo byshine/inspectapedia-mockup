e1c1dc34f021ff611cf78c25f41ecec98095dbd7
aaron <aaron@countable.ca>
2018-04-06 10:17:10 -0700

    responsiveness

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$( document ).ready( function (e) {
+    if ( $( window ).width() <= 700 ) {
+        $("#sidebar-caret").removeClass("hidden");
+    }
+});
+
+$( window ).resize( function( e ) {
+} );
+
