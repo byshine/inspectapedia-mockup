7d898e34b51c048fb96edb762b084bd4ca771f40
aaron <aaron@countable.ca>
2018-04-06 10:33:10 -0700

    sidebar simple responsive

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 $( document ).ready( function (e) {
     if ( $( window ).width() <= 700 ) {
         $("#sidebar-caret").removeClass("hidden");
+        
+        $("#sidebar-caret").click( function(e) {
+            if ( $("#sideBar-list").hasClass("mobile-side-nav") ) {
+                $("#sideBar-list").removeClass("mobile-side-nav");
+            } else {
+                $("#sideBar-list").addClass("mobile-side-nav");
+            }
+
+        });
     }
 });
 
